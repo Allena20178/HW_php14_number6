4fb34580383e8826671bdf6245a1941df6a52acc
Alona Katrechko <alenalogvinova20134@gmail.com>
2021-08-01 09:39:48 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$arr = [1,2,3,4,5,12,25,6,8,18];
+//function search_max($arr){
+   // if($arr == 1){return $arr[0];}
+    //if ($arr[0] > search_max($arr)){return $arr[0];}
+// else
+//{return search_max($arr);}}
+$max = max($arr);
+echo ($max);
